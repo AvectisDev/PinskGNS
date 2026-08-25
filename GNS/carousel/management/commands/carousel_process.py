3df08e6724a23a36832704ee0f12a05fcd629757
akondratenko <akondratenko@avectis.by>
2026-08-25 18:03:44 +0300

    исправлен вызов carousel_process

--- GNS/carousel/management/commands/carousel_process.py
+++ GNS/carousel/management/commands/carousel_process.py
@@ -1,11 +1,25 @@
-from django.core.management.base import BaseCommand
+import os
 import subprocess
+import sys
+from pathlib import Path
+
+from django.core.management.base import BaseCommand
+
+
+DJANGO_PROJECT_DIR = Path(__file__).resolve().parents[3]
 
 
 class Command(BaseCommand):
     help = 'Запуск main.py - приложения обработки данных с постов наполнения УНБ'
 
     def handle(self, *args, **kwargs):
-        subprocess.Popen([
-            'python', '-m', 'carousel.management.commands.carousel.main'
-        ])
+        env = os.environ.copy()
+        subprocess.Popen(
+            [
+                sys.executable,
+                '-m',
+                'carousel.management.commands.carousel.main',
+            ],
+            env=env,
+            cwd=DJANGO_PROJECT_DIR,
+        )
